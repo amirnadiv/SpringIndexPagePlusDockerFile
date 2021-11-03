e6d342d9d9ff31fb297757265f91b322af824bf2
amirnadiv <amirnadiv@gmail.com>
2021-11-04 00:47:25 +0200

    Initial commit

---
+++ src/main/java/com/example/amirnadivspringwithdocker/AmirNadivSpringWithDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.amirnadivspringwithdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmirNadivSpringWithDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmirNadivSpringWithDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/amirnadivspringwithdocker/AmirNadivSpringWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amirnadivspringwithdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmirNadivSpringWithDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
